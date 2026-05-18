5e54573a28f747d7f34da30a80cb3b209c2e21eb
Chethiya4 <chethiyasamaradiwakara@gmail.com>
2026-05-18 15:08:40 +0530

    Update CI pipeline to run Calculator

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -2,10 +2,25 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator Cal = new Calculator();
         
+        
         if (Cal.add(2, 3) == 5) {
-            System.out.println("Test Passed");
+            System.out.println("Addition Test Passed");
         } else {
-            System.out.println("Test Failed");
+            System.out.println("Addition Test Failed");
+        }
+
+        
+        if (Cal.subtract(10, 4) == 6) {
+            System.out.println("Subtraction Test Passed");
+        } else {
+            System.out.println("Subtraction Test Failed");
+        }
+
+        
+        if (Cal.multiply(3, 4) == 12) {
+            System.out.println("Multiplication Test Passed");
+        } else {
+            System.out.println("Multiplication Test Failed");
         }
     }
 }
